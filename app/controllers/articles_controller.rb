f9cecc1b71a380bd9827633ad59667948d5c90ca
aqutw <aquarian.ex@gmail.com>
2015-09-06 19:46:42 +0800

    finish articles#show articles#create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,17 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show]
+
   def index
+  end
+
+  def show
   end
 
   def new
     @article = Article.new
   end
 
-  def cretae
+  def create
     @article = Article.new(article_params)
     if @article.save
       redirect_to @article
@@ -16,6 +21,10 @@ class ArticlesController < ApplicationController
   end
 
   private
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :content)
